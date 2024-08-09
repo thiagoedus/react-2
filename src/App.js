14cd9702c95e21f08142296a8a129d77154b948a
Thiago Santos <playsd395@gmail.com>
2024-08-09 10:13:52 -0300

    Hooks - useCallback

--- src/App.js
+++ src/App.js
@@ -1,80 +1,32 @@
+import p from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return <button onClick={() => incrementButton(100)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: p.func,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
 
-  useEffect(() => {
-    console.log('C1: ', counter, 'C2: ', counter2);
-  }, [counter, counter2]);
+  console.log('Pai, renderizou');
 
   return (
     <div className="App">
       <h1>Teste 3</h1>
-      <h1>
-        C1: {counter} C2: {counter2}
-      </h1>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increment
-      </button>
-
-      <button
-        onClick={() => {
-          setCounter2(counter2 + 1);
-        }}
-      >
-        Increment (2)
-      </button>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     reverse: false,
-//   };
-
-//   handleClick = () => {
-//     const { reverse } = this.state;
-//     this.setState({ reverse: !reverse });
-//   };
-
-//   render() {
-//     const { reverse } = this.state;
-//     const reverseClass = reverse ? 'reverse' : '';
-
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-//           <button type="button" onClick={this.handleClick}>
-//             Reverse {reverseClass}
-//           </button>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
